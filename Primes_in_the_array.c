c6d19d20ed5f1d86e86e20159d6e8ed697c17bd2
21A91A05H3 <97424746+21A91A05H3@users.noreply.github.com>
2022-07-07 10:48:21 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],c=0,fact,j;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         fact=0;         for(j=1;j<=arr[i];j++)         {             if(arr[i]%j==0)             {                 fact++;             }         }         if(fact==2)         {             c++;         }     }     printf("%d",c); }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],c=0,fact,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        fact=0;
+        for(j=1;j<=arr[i];j++)
+        {
+            if(arr[i]%j==0)
+            {
+                fact++;
+            }
+        }
+        if(fact==2)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
